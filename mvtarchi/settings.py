975fcc116c321ace46290bf1c53177770582b0be
theprerakshah <prerakoct2000@gmail.com>
2022-07-11 23:25:43 +0530

    changes in static file mapping

--- mvtarchi/settings.py
+++ mvtarchi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +130,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static_root','static')
--- mvtarchi/urls.py
+++ mvtarchi/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from mvtexample import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
      path('admin/', admin.site.urls),
      path('', include('mvtexample.urls'))
    
   
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
